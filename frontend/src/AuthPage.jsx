749ae08b27e7ce6fd7fd32f61167087b5ad56c37
José Gabriel de Lima Santos Costa <josetropilo@gmail.com>
2023-12-01 16:07:45 -0300

    Alterações

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -16,15 +16,15 @@ const AuthPage = (props) => {
     return (
       <div className="background">
         <form onSubmit={onSubmit} className="form-card">
-          <div className="form-title">Welcome 👋</div>
+          <div className="form-title">Bem vindo! 👋</div>
   
-          <div className="form-subtitle">Set a username to get started</div>
+          <div className="form-subtitle">Digite um nome de usuário para começar :D</div>
   
           <div className="auth">
-            <div className="auth-label">Username</div>
+            <div className="auth-label">Usuário</div>
             <input className="auth-input" name="username" />
             <button className="auth-button" type="submit">
-              Enter
+              Entrar
             </button>
           </div>
         </form>
