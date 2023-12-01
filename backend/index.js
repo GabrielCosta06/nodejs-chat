8a41934dc82b40b8ffc21845071fc3e05bd2ee10
José Gabriel de Lima Santos Costa <josetropilo@gmail.com>
2023-12-01 15:58:30 -0300

    Código Adicionado

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "08401d65-bbb0-479b-a754-7b4268cc39f1" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,12 @@
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic('a974b927-3a81-442c-8151-6fec96048062', props.user.username, props.user.secret);
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+        </div>
+    )
+}
+
+export default ChatsPage
